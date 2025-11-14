d50f7c8753f1944e280815eeb80ea8af13250846
saurabhsequel <saurabh.singh@sequelstring.com>
2025-11-14 11:59:02 +0530

    first commit

--- utils/mailVerification.js
+++ utils/mailVerification.js
@@ -16,23 +16,23 @@ exports.mailerFun = async (email, name, userId) => {
     },
   });
   let info = await transporter.sendMail({
-    from: '"Books Care" <process.env.FROM>', // sender address
+    from: '"Globalboardworld-10q" <process.env.FROM>', // sender address
     to: `${name} <${email}>`, // list of receivers /name /email
     subject: "verification mail ", // Subject line
     text: "verification mail", // plain text body
     html: `<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
         <div style="margin:50px auto;width:70%;padding:20px 0">
           <div style="border-bottom:1px solid #eee">
-            <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Books Care</a>
+            <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Globalboardworld-10q</a>
           </div>
           <p style="font-size:1.1em">Hi,</p>
-          <p>Thank you for choosing Books Care. Use the following Link to complete your Sign Up procedures</p>
+          <p>Thank you for choosing Globalboardworld-10q. Use the following Link to complete your Sign Up procedures</p>
           <h2 style="background: #4044ee;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">
              <a href=${baseurl + "verify/" + userId + "/" + uniqueString
       } style=" color:white"> verify </a> 
               
               </h2>
-          <p style="font-size:0.9em;">Regards,<br />Books Care</p>
+          <p style="font-size:0.9em;">Regards,<br />Globalboardworld-10q</p>
           <hr style="border:none;border-top:1px solid #eee" />
           <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
             
